37907e58dd3b1db4f2925c47f029164b6666168a
lazygeniuslanz <lanzgio7@gmail.com>
2019-02-18 10:33:19 +0800

    scroll fix

--- js/scroll.js
+++ js/scroll.js
@@ -7,14 +7,14 @@ function changeBackground() {
     var div_top = $('#description').offset().top;
 
     if (window_top > (div_top - 1000)) {
-        $('body').css('background', 'url(css/img/cover3.jpg) center center no-repeat fixed');
+        $('body').css('background', 'url(https://lazygeniuslanz.github.io/Puppies/css/img/cover3.jpg) center center no-repeat fixed');
         $('body').css('-webkit-background-size', 'cover');
         $('body').css('-moz-background-size', 'cover');
         $('body').css('-o-background-size', 'cover');
         $('body').css('background-size', 'cover');
     }
     else {
-        $('body').css('background', 'url(css/img/cover1.jpg) center center no-repeat fixed');
+        $('body').css('background', 'url(https://lazygeniuslanz.github.io/Puppies/css/img/cover1.jpg) center center no-repeat fixed');
         $('body').css('-webkit-background-size', 'cover');
         $('body').css('-moz-background-size', 'cover');
         $('body').css('-o-background-size', 'cover');
