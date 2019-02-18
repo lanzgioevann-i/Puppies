e6ba6ca4c6731877384e7514cb1ff08a5a6b8dc6
lazygeniuslanz <lanzgio7@gmail.com>
2019-02-18 10:38:02 +0800

    scroll

--- js/scroll.js
+++ js/scroll.js
@@ -7,17 +7,19 @@ function changeBackground() {
     var div_top = $('#description').offset().top;
 
     if (window_top > (div_top - 1000)) {
-        $('body').css('background', 'url(https://lazygeniuslanz.github.io/Puppies/css/img/cover3.jpg) center center no-repeat fixed');
+        $('body').css('background', 'url(https://lazygeniuslanz.github.io/Puppies/css/img/cover3.jpg) center center no-repeat');
         $('body').css('-webkit-background-size', 'cover');
         $('body').css('-moz-background-size', 'cover');
         $('body').css('-o-background-size', 'cover');
         $('body').css('background-size', 'cover');
+        $('body').css('background-attachment', 'fixed');
     }
     else {
-        $('body').css('background', 'url(https://lazygeniuslanz.github.io/Puppies/css/img/cover1.jpg) center center no-repeat fixed');
+        $('body').css('background', 'url(https://lazygeniuslanz.github.io/Puppies/css/img/cover1.jpg) center center no-repeat');
         $('body').css('-webkit-background-size', 'cover');
         $('body').css('-moz-background-size', 'cover');
         $('body').css('-o-background-size', 'cover');
         $('body').css('background-size', 'cover');
+        $('body').css('background-attachment', 'scroll');
     }
 }
