0b4e799f934b85478c6cb5068175ced4714b6d59
lazygeniuslanz <lanz.irlanda@gmail.com>
2019-03-10 10:18:28 +0800

    big revision

--- js/works.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('appBody', []);
-app.controller('WorksController', function($scope, $http){
-	$http.get('https://raw.githubusercontent.com/lazygeniuslanz/newsite/master/data/works.json').success(function(data){
-		$scope.works = data;
-		$(".works-container").css("background-image", data[0].imgSrc);
-		
-		console.log("WorksController loaded");
-	});
-});
