96dc55c6ea1b6e12f995a738e2fb2a32724d4dac
lazygeniuslanz <lanz.irlanda@gmail.com>
2019-03-10 15:25:22 +0800

    tst

--- js/functions.js
+++ js/functions.js
@@ -9,25 +9,50 @@
     }
 }
 
+/* HEADER */
+jQuery(document).ready(function($){
+	// Defining a function to set size for #header 
+    function fullscreen(){
+        jQuery('#header-container').css({
+            width: jQuery(window).width(),
+            height: jQuery(window).height()
+        });
+    }
+  
+    fullscreen();
+
+	// Run the function in case of window resize
+	jQuery(window).resize(function() {
+		fullscreen();         
+	});
+
+	//Mobile Browser Fix (Chrome's Address bar)
+	if ($('body').hasClass('mobile')) {
+		var viewportHeight = $('#header-container').innerHeight();
+		$('#header-container').css({ height: viewportHeight });
+	}
+
+});
+
 var w = window.innerWidth;
-    var h = window.innerHeight;
-    var o = 'landscape';
-    var i = 1;
+var h = window.innerHeight;
+var o = 'landscape';
+var i = 1;
 
-    if(w >= h){
-        o = 'landscape'
-    }
-    else{
-        o = 'portrait'
-    }
-    
-    $('body').css('background', 'url(img/cover/' + o + '/1.jpg) center center no-repeat');
-    $('body').css('-webkit-background-size', 'cover');
-    $('body').css('-moz-background-size', 'cover');
-    $('body').css('-o-background-size', 'cover');
-    $('body').css('background-size', 'cover');
-    $('body').css('background-attachment', 'fixed');
+if(w >= h){
+	o = 'landscape'
+}
+else{
+	o = 'portrait'
+}
 
-    var iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
-    if(iOS) $('body').css('background-attachment', 'fixed');
+$('body').css('background', 'url(img/cover/' + o + '/1.jpg) center center no-repeat');
+$('body').css('-webkit-background-size', 'cover');
+$('body').css('-moz-background-size', 'cover');
+$('body').css('-o-background-size', 'cover');
+$('body').css('background-size', 'cover');
+$('body').css('background-attachment', 'fixed');
+
+var iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
+if(iOS) $('body').css('background-attachment', 'fixed');
 	
